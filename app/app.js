393e293986ff5b61adc518b69d03f112c5c4e92d
gusgard <gusgard@gmail.com>
2014-11-13 19:29:15 -0200

    change the name of the app.

--- app/app.js
+++ app/app.js
@@ -1,12 +1,22 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('gameOfDrones', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'gameOfDrones.view1',
+  'gameOfDrones.view2',
+  // 'myApp.version',
+  'playerControllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
+'use strict';
+
+var playerControllers = angular.module('playerControllers', []);
+
+playerControllers.controller("ReviewController", function(){  
+	this.playerOne = '';
+	this.playerTwo = '';
+});
